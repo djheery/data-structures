26423c7b40531e54cef9c06a6d27c58ba28d6870
Daniel Heery <danieljheery@gmail.com>
2023-12-15 12:04:50 +0000

    ArrayList shit

--- lists/array-list/array-list.c
+++ lists/array-list/array-list.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 
 #define INITIAL_CAPACITY 5 
+#define RESIZE_LOAD_FACTOR 0.7
 
 typedef struct {
   int* array; 
@@ -13,26 +14,36 @@ typedef struct {
 
 // ArrayList user operations
 void show(ArrayList* arrayList); 
-void find(ArrayList* arrayList); 
 void get(ArrayList* arrayList); 
-void insertIdx(ArrayList* arrayList); 
-void removeIdx(ArrayList* arrayList); 
+void insertAtIdx(ArrayList* arrayList); 
+void removeAtIdx(ArrayList* arrayList); 
 void push(ArrayList* arrayList); 
 void pop(ArrayList* arrayList); 
 void shift(ArrayList* arrayList); 
 void unshift(ArrayList* arrayList); 
 void size(ArrayList* arrayList); 
+void capacity(ArrayList* arrayList);
 void loadFactor(ArrayList* arrayList); 
+void clear(ArrayList* arrayList); 
+void isEmpty(ArrayList* arrayList); 
+void indexOf(ArrayList* arrayList); 
+void trimToSize(ArrayList* arrayList); 
+void reverse(ArrayList* arrayList); 
+void contains(ArrayList* arrayList); 
+void subList(ArrayList* arrayList); 
 
 // Array List utility Operations 
 float getLoadFactor(ArrayList* arrayList); 
 bool getIsFull(ArrayList* arrayList); 
 bool getIsEmpty(ArrayList* arrayList); 
-
+bool getContains(ArrayList* arrayList, int numToCheck); 
+void swap(ArrayList* arrayList, int idx1, int idx2); 
+int getIdx(ArrayList* arrayList, int idx); 
 
 // General Program Methods
 void programLoop(); 
 void printMenu(); 
+void printSubListOptions(); 
 void resize(ArrayList* arrayList); 
 ArrayList initializeArrayList(); 
 int getUserInput(char textPrompt[]); 
@@ -70,6 +81,28 @@ void programLoop() {
         break; 
       case 7 : 
         break; 
+      case 8 : 
+        break; 
+      case 9 : 
+        break; 
+      case 10 : 
+        break;
+      case 11 : 
+        break; 
+      case 12 : 
+        break; 
+      case 13 : 
+        break; 
+      case 14 : 
+        break; 
+      case 15 : 
+        break; 
+      case 16 : 
+        break; 
+      case 17 : 
+        break; 
+      case 18: 
+        break; 
       case 0 : 
         free(arrayList.array); 
         printf("Exiting the program...\n"); 
@@ -97,6 +130,15 @@ void printMenu() {
   printf("[7] Unshift an Item from the array\n"); 
   printf("[8] Show the current size of the array\n"); 
   printf("[9] Show the current capacity of the array\n"); 
+  printf("[10] Show the current load factor of the array\n"); 
+  printf("[11] Show if the array is empty\n"); 
+  printf("[12] Show if the array is Full\n"); 
+  printf("[13] Show the index of an item\n"); 
+  printf("[14] Trim array to size\n"); 
+  printf("[15] Reverse the array\n"); 
+  printf("[16] See if the array contains an item\n"); 
+  printf("[17] Create a see a sublist from provided options\n");
+  printf("[18] Populate the array with random elements\n"); 
   printf("[0] Exit the program\n\n"); 
 }
 
@@ -118,6 +160,68 @@ int getUserInput(char textPrompt[]) {
   return 0;  
 }
 
-bool validateUserInput(char *pointer) {
+bool validateUserInput(char* pointer) {
   return false;  
+}
+
+float getLoadFactor(ArrayList* arrayList) {
+  float size = (float) arrayList->size; 
+  float capacity = (float) arrayList->currentCapacity;
+  return size / capacity; 
+}
+
+int getIdx(ArrayList* arrayList, int idx) {
+  if(idx > (arrayList->size - 1)) {
+    return -1; 
+  }
+
+  return arrayList->array[idx];
+}
+
+bool getContains(ArrayList* arrayList, int numToCheck) {
+  return false; 
+}
+
+void show(ArrayList* arrayList) {
+  
+}
+
+void get(ArrayList* arrayList) {
+  
+}
+
+void insertAtIdx(ArrayList* arrayList) {
+  
+}
+
+void removeAtIdx(ArrayList* arrayList) {
+  
+}
+
+void push(ArrayList* arrayList) {
+  
+}
+
+void pop(ArrayList* arrayList) {
+  
+}
+
+void shift(ArrayList* arrayList) {
+  
+}
+
+void unshift(ArrayList* arrayList) {
+  
+}
+
+void size(ArrayList* arrayList) {
+  
+}
+
+void capacity(ArrayList* arrayList) {
+  
+}
+
+void subList(ArrayList* arrayList) {
+  
 }
