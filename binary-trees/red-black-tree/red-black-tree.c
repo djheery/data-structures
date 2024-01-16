986c9fa757058f60374ce19f94290f26a7d29d7e
Daniel Heery <danieljheery@gmail.com>
2024-01-16 16:22:36 +0000

    Start of Red Black Tree - I feel like I am on the presipice of feeling stupid

---
+++ binary-trees/red-black-tree/red-black-tree.c
@@ -0,0 +1,18 @@
+#include <stdlib.h> 
+#include <stdio.h>
+#include <stdbool.h>
+
+#define RED 1; 
+#define BLACK 0; 
+
+typedef struct {
+  int data; 
+  int color; 
+  void* left; 
+  void* right; 
+  void* parent; 
+} Node; 
+
+int main() {
+  return 0; 
+}
