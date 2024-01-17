9610a77aa053c03436ed0e4ef27c30331736d613
Daniel Heery <danieljheery@gmail.com>
2024-01-17 17:27:09 +0000

    Boiler Plate for Red Black Tree

--- binary-trees/red-black-tree/red-black-tree.c
+++ binary-trees/red-black-tree/red-black-tree.c
@@ -4,6 +4,7 @@
 
 #define RED 1; 
 #define BLACK 0; 
+#define QUEUE_CAPACITY 30; 
 
 typedef struct {
   int data; 
@@ -12,6 +13,39 @@ typedef struct {
   void* right; 
   void* parent; 
 } Node; 
+
+typedef struct {
+  Node* head;
+  int size; 
+} RedBlackTree; 
+
+typedef struct {
+  Node** queue; 
+  int size; 
+  int capacity; 
+  int front; 
+  int rear; 
+} CircularQueue; 
+
+RedBlackTree initialize_tree(); 
+Node* initialize_node(int node_data); 
+void free_tree(RedBlackTree* tree); 
+
+void insert(Node* root, int node_data); 
+
+void delete(Node* root, int node_to_delete); 
+
+bool search(Node* root, int node_data); 
+
+int size(Node* root, int current_sum); 
+
+void left_rotation(Node* root); 
+void right_rotation(Node* root); 
+
+int height(Node* root); 
+int get_max(int a, int b);
+
+int invert(Node* root); 
 
 int main() {
   return 0; 
