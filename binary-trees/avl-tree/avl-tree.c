17d10f6bb0dfafcca32f5311f327db422a0a1b7d
Daniel Heery <danieljheery@gmail.com>
2024-03-05 06:57:24 +0000

    queue boilerplate

--- binary-trees/avl-tree/avl-tree.c
+++ binary-trees/avl-tree/avl-tree.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define QUEUE_CAPACITY 50
+
 typedef struct Node Node; 
 
 typedef struct Node {
@@ -17,8 +19,9 @@ typedef struct {
 } AVLTree; 
 
 typedef struct {
+  Node** queue; 
   uint16_t front; 
-  uint16_t rear;
+  int16_t rear;
   uint16_t capacity; 
   uint16_t length; 
 } CircularQueue; 
@@ -26,3 +29,63 @@ typedef struct {
 int main() {
   return 0; 
 }
+
+/**
+ * ====================
+ * || Circular Queue ||
+ * ====================
+ */
+
+CircularQueue initialize_queue() {
+  CircularQueue circ_queue; 
+
+  Node** queue = (Node**) malloc(sizeof(Node) * QUEUE_CAPACITY); 
+
+  if (queue == NULL) {
+    printf("Something went wrong allocating the queue\n\n");
+    exit(EXIT_FAILURE);
+  }
+
+  circ_queue.capacity = QUEUE_CAPACITY; 
+  circ_queue.front = 0; 
+  circ_queue.rear = -1; 
+  circ_queue.length = 0; 
+
+  return circ_queue; 
+}
+
+void free_queue(CircularQueue* circ_queue) {
+  
+  if (circ_queue == NULL) {
+    printf("Queeu should not be NULL when calling free queue\n\n"); 
+    exit(EXIT_FAILURE); 
+  }
+
+  free(circ_queue->queue); 
+  circ_queue = NULL; 
+}
+
+void enqueue(CircularQueue* circ_queue, Node* node) {
+
+  if (circ_queue == NULL) {
+    printf("Queue should not be NULL when calling enqueue\n"); 
+    exit(EXIT_FAILURE); 
+  }
+
+  if (node == NULL) {
+    printf("Node* should not be NULL when calling enqueue\n"); 
+    exit(EXIT_FAILURE); 
+  }
+
+  if (circ_queue->length == circ_queue->capacity) {
+    printf("Cannot enqueue another item due to queue reaching capacity\n\n"); 
+    exit(EXIT_FAILURE); 
+  }
+
+  circ_queue->rear += 1;
+  circ_queue->queue[circ_queue->rear] = node; 
+  circ_queue->length += 1;
+
+}
+
+
